1f5ed6016f51d4c6d3a6258a41134ac6746c73b9
beatrizsmerino <beatrizsmerino@gmail.com>
2019-05-19 01:06:03 +0200

    Init proyect

---
+++ main.js
@@ -0,0 +1,5 @@
+const urls = {
+    4: 'https://api.jsonbin.io/b/5cd2137a4c004c0eb4950d03',
+    6: 'https://api.jsonbin.io/b/5cd213c964d4fc359ead3a5a',
+    8: 'https://api.jsonbin.io/b/5cd213f6c07f283511e1c251',
+};
